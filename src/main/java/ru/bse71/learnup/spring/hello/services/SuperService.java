d0414f7127db6ca97e338700edb62df0b9acbcc0
bse71 <bse71@bse71.ru>
2021-08-05 13:33:04 +0300

    param + ctx

--- src/main/java/ru/bse71/learnup/spring/hello/services/SuperService.java
+++ src/main/java/ru/bse71/learnup/spring/hello/services/SuperService.java
@@ -1,5 +1,9 @@
 package ru.bse71.learnup.spring.hello.services;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -9,16 +13,23 @@ import java.util.ResourceBundle;
  * @author bse71
  * Created on 02.08.2021
  */
-public class SuperService {
+public class SuperService implements ApplicationContextAware {
 
     private Logger logger;
+    private ApplicationContext ctx;
 
     public SuperService(Logger logger) {
         this.logger = logger;
     }
 
     public void doWork() {
-        final ResourceBundle resources = ResourceBundle.getBundle("text", Locale.ITALY);
-        logger.log(resources.getString("startworking"));
+        final Locale locale = Locale.US;
+        logger.log(ctx.getMessage("hello", new String[]{"LearnUp"}, locale));
+        logger.log(ctx.getMessage("startworking", new Object[0], locale));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.ctx = applicationContext;
     }
 }
